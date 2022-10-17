92f13ce62f91541107e9017c833fca8c4ea3cf6f
Jaw Knee <mwangijohngitau1@gmail.com>
2022-10-17 09:59:55 +0300

    loop, pause, generate

--- odd.py
+++ odd.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-***datetime is a standard library***
+
 from datetime import datetime
-""" The first datetime is the name of the standard library module to import the reusable code from
-    The second datetime is the name of the submodule
-"""
+import random
+import time
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
-right_this_minute = datetime.today().minute
+for i in range(5):
+    right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("This minute seems to be odd.")
-else:
-    print("Not an odd minute.")
+    if right_this_minute in odds:
+        print("This minute seems to be odd.")
+    else:
+        print("Not an odd minute.")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
